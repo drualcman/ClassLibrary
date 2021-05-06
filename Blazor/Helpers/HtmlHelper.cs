93c8fffaece18750da77c57d53b7cb9c88f940e7
Sergi Ortiz Gómez <drualcman@msn.com>
2021-05-07 07:28:38 +0800

    add extension in string .ToHtml() to get a text with html markup

---
+++ Blazor/Helpers/HtmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ClassLibrary.Helpers
+{
+    public static class HtmlHelper
+    {
+        public static MarkupString ToHtml(this string content)
+        {
+            MarkupString result = new MarkupString(content);
+            return result;
+        }
+    }
+}
+
