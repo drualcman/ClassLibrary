3cef5a98dea7294b948099fe1ccaa71414b52fc7
Sergi Ortiz Gómez <drualcman@msn.com>
2021-06-09 06:45:52 +0800

    Add default services to this project fro can be used in all the blazor projects

---
+++ Blazor/Services/DefaultServices.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary.Logger;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+using System.Net.Http;
+
+namespace ClassLibrary.Service
+{
+    public class DefaultServices : IDefaultServices
+    {
+        public DefaultServices(HttpClient Client, 
+            NavigationManager nav, 
+            IJSRuntime js)
+        {
+            this.Client = Client;
+            this.Navigation = nav;
+            this.JsRuntime = js;
+        }
+
+        public HttpClient Client { get; }
+        public NavigationManager Navigation { get; }
+        public IJSRuntime JsRuntime { get; }
+    }
+}
---
+++ Blazor/Services/IDefaultServices.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
+
+namespace ClassLibrary.Service
+{
+    /// <summary>
+    /// Service interface for our DefaultServices service
+    /// </summary>
+    public interface IDefaultServices
+    {
+        public HttpClient Client { get; }
+        public NavigationManager Navigation { get; }
+        public IJSRuntime JsRuntime { get; }
+    }    
+}
