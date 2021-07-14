724e56c7592ddeaf1a82bc7aa0c8b63078cf84e9
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-14 08:17:43 +0800

    Add search on table component. Remove a lot duplicate code in propios I don't know why have there :O

---
+++ Blazor/Controls/SearchOnTable.razor.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary.Service;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Controls
+{
+    public partial class SearchOnTable
+    {
+        [Inject]
+        public IDefaultServices Services { get; set; }
+
+        [Parameter]
+        public string Table { get; set; }
+
+        string SearchText;
+
+        #region methods
+        async Task FilterTable()
+        {
+            await Services.JsRuntime.InvokeVoidAsync("$p.SearchInTable", Table, SearchText);
+        }
+
+        public async Task Clear()
+        {
+            SearchText = "";
+            await FilterTable();
+            StateHasChanged();
+        }
+        #endregion
+    }
+}
