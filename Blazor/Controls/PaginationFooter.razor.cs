3582789520b122f65329234c88de9904db85288c
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-20 13:29:50 +0800

    add pagination footer to use with a PagedList object

---
+++ Blazor/Controls/PaginationFooter.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Controls
+{
+    public partial class PaginationFooter
+    {
+        #region basics
+        [Parameter] public int TotalPages { get; set; }
+        [Parameter] public int CurrentPage { get; set; }
+        [Parameter] public EventCallback<int> OnChange { get; set; }
+
+        void ToPage(int page)
+        {
+            OnChange.InvokeAsync(page);
+        }
+        #endregion
+
+        #region Personalize
+        [Parameter] public string ContainerCss { get; set; }
+        [Parameter] public string ElementCss { get; set; }
+        #endregion
+    }
+}
