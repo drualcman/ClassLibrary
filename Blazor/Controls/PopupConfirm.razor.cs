58d786b635c38119557d3390b46894b83605d280
Sergi Ortiz Gómez <drualcman@msn.com>
2021-08-02 15:23:10 +0800

    small refactor to standarize a bit the show popup confirm

--- Blazor/Controls/PopupConfirm.razor.cs
+++ Blazor/Controls/PopupConfirm.razor.cs
@@ -12,15 +12,32 @@ namespace ClassLibrary.Controls
         public EventCallback ClosePopup { get; set; }
 
         [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
         public bool IsShow { get; set; }
 
         [Parameter]
         public string LoadingBtn { get; set; }
 
-        protected override Task OnParametersSetAsync()
+        [Parameter]
+        public bool IsLoading { get; set; }
+
+        [Parameter]
+        public string ActiveCss { get; set; }
+
+        protected override void OnParametersSet()
         {
             LoadingBtn = LoadingBtn ?? "";
-            return base.OnParametersSetAsync();
+            ActiveCss = ActiveCss ?? "is-active";
+            if (LoadingBtn == "popupsubmit")
+            {
+                IsLoading = true;
+            }
+            else
+            {
+                if (IsLoading) LoadingBtn = "cst-loading";
+            }
         }
 
         void Close() => ClosePopup.InvokeAsync();
