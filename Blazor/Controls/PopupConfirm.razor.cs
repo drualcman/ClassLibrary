4668a87e0958cb56ffa63e813c8100993dabcc38
Kenneth Dimabuyo <bjergken06@gmail.com>
2021-08-02 14:34:05 +0800

    move the popup confirm in class library

---
+++ Blazor/Controls/PopupConfirm.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Controls
+{
+    public partial class PopupConfirm
+    {
+        [Parameter]
+        public EventCallback Agreed { get; set; }
+
+        [Parameter]
+        public EventCallback ClosePopup { get; set; }
+
+        [Parameter]
+        public bool IsShow { get; set; }
+
+        [Parameter]
+        public string LoadingBtn { get; set; }
+
+        protected override Task OnParametersSetAsync()
+        {
+            LoadingBtn = LoadingBtn ?? "";
+            return base.OnParametersSetAsync();
+        }
+
+        void Close() => ClosePopup.InvokeAsync();
+
+        void Continue() => Agreed.InvokeAsync();
+    }
+}
