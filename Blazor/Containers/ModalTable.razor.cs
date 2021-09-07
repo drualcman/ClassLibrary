4359d6af364ded18d38fe4f9d5dc34e477adf337
DaryllFelipe <rlfelipe07@gmail.com>
2021-09-07 14:10:41 +0800

    Added container wrapper for modal tables

---
+++ Blazor/Containers/ModalTable.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Containers
+{
+    public partial class ModalTable
+    {
+        #region Properties
+        [Parameter]
+        public string TableId { get; set; }
+        [Parameter]
+        public string TableTitle { get; set; }
+        [Parameter]
+        public List<string> DisplayNames { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public EventCallback OnSelect { get; set; }
+        [Parameter]
+        public EventCallback OnClose { get; set; }
+        [Parameter]
+        public EventCallback OnUpdate { get; set; }
+        #endregion
+        #region Methods
+        void CloseModal() => OnClose.InvokeAsync();
+
+        void UpdateList() => OnUpdate.InvokeAsync();
+
+        void Select()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Blazor/Containers/SelectTableRow.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Containers
+{
+    public partial class SelectTableRow<T>
+    {
+        [Parameter]
+        public List<T> Items { get; set; }
+        [Parameter]
+        public RenderFragment<T> Body { get; set; }
+    }
+}
