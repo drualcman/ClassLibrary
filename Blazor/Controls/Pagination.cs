bd4147612fcd7d2b2ce86a958c58191d327df9ef
Sergi Ortiz Gómez <drualcman@msn.com>
2021-11-21 16:02:10 +0800

    add pagination class specialized for web assembly. No mis MVC here.

---
+++ Blazor/Controls/Pagination.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Controls.PaginationLists
+{
+    public class PagedList<T> : List<T>
+    {
+        #region properties
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        #endregion
+
+        #region constructor
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+        #endregion
+
+        #region Methods
+        public static PagedList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            int count = source.Count();
+            List<T> items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+        public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+        {
+            int count = source.Count();
+            List<T> items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+
+        public static PagedList<T> ToPagedList(List<T> source, int pageNumber, int pageSize)
+        {
+            int count = source.Count();
+            List<T> items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+        #endregion
+    }
+}
