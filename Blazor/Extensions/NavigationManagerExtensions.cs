d65019d5319bba31608f70a4a2cd1327a0dc6371
Sergi Ortiz Gómez <drualcman@msn.com>
2022-02-06 12:22:35 +0800

    add extension to read query strings

---
+++ Blazor/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static string GetQueryString(this NavigationManager navManager, string key)
+        {
+            Uri uri = navManager.ToAbsoluteUri(navManager.Uri);
+            return QueryHelpers.ParseNullableQuery(uri.Query)[key];
+        }
+    }
+}
